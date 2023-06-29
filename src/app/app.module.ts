0f425f3dcdd24e9f7e235c2a2ad8d049e4c292ab
María Espartero García <133367024+maesga1@users.noreply.github.com>
2023-06-29 15:40:12 +0200

    Install angular material and agregate mat toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [
@@ -14,6 +15,8 @@ import { FooterComponent } from './footer/footer.component';
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
+    MatIconModule,
     BrowserAnimationsModule
   ],
   providers: [],
